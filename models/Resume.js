6a91924359ce3d0d92eb25cedc87c1e3acb29efd
Chioma <@francachioma02@gmail.com>
2025-03-06 20:28:09 +0100

    Added new file to feature/AI-HR

--- models/Resume.js
+++ models/Resume.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const ResumeSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    
     name: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: String },
